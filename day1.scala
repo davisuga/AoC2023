b09deee6861a81598703e10bf7e073b7118793d6
davisuga <daviciencia1@gmail.com>
2023-12-03 11:44:17 -0300

    Update file path in main function

--- day1.scala
+++ day1.scala
@@ -1,12 +1,14 @@
+package com.davisuga.day1
+
 def getFirstAndLastInts(line: String) =
   val ints = line.flatMap(_.toString.toIntOption)
   (ints.headOption, ints.lastOption) match
     case (Some(first), Some(last)) => (s"${first}${last}").toInt
     case _                         => 0
 
-@main def main = print:
+@main def main =
   io.Source
-    .fromFile("day1input.txt")
+    .fromFile("inputs/day1input.txt")
     .getLines
     .map(getFirstAndLastInts)
     .reduce(_ + _)
